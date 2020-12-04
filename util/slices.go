5ce619471c868aeb68f2617bc3e7cee6e4948641
A_D <aunderscored@gmail.com>
2020-12-04 14:44:50 +0200

    Added some slice utils

---
+++ util/slices.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func SliceContains(slice interface{}, value interface{}) bool {
+	sType := reflect.TypeOf(slice)
+	vType := reflect.TypeOf(value)
+	if sType.Kind() != reflect.Slice {
+		panic(fmt.Sprintf("SliceContains passed non slice %T", slice))
+	}
+
+	if sType.Elem().Kind() != vType.Kind() {
+		panic(fmt.Sprintf("SliceContains passed non-matching slice and value types %T, %T", slice, value))
+	}
+
+	rSlice := reflect.ValueOf(slice)
+	for i := 0; i < rSlice.Len(); i++ {
+		v := rSlice.Index(i)
+		if reflect.DeepEqual(v.Interface(), value) {
+			return true
+		}
+	}
+	return false
+}
+
+func StringSliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
